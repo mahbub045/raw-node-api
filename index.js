8bef5053f3eebd0a7d1b15baa1bd918053fee2a5
mahbub045 <mahbubrmn45@gmail.com>
2026-05-04 17:43:29 +0600

    fix: update package resolutions from Yarn to npm registry for consistency
    - Changed package resolutions in yarn.lock to point to npm registry instead of Yarn registry for various packages including @eslint/eslintrc, @humanwhocodes/config-array, acorn, ajv, ansi-colors, chalk, eslint, and others.
    - Ensured compatibility by adjusting version ranges for eslint and eslint-config-prettier.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const fs = require('fs');
+// dependencies
+const http = require('http');
 
-const a = fs.readFileSync('index.js', 'utf-8');
-console.log(a);
+// app object - module scaffolding
+const app = {};
 
-fs.writeFileSync('index.js', 'console.log("Hello, world!");');
+// configuration
+app.config = {
+  port: 3000,
+};
 
-const b = fs.readFileSync('index.js', 'utf-8');
+//  create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`server is listening on port ${app.config.port}`);
+  });
+};
+
+// handle request response
+app.handleReqRes = (req, res) => {
+  res.end('Hello World! Welcome to my server.');
+};
+
+// start the server
+app.createServer();
