69637710cf54c4e732c8020854b840551acd57bb
mahbub045 <mahbubrmn45@gmail.com>
2026-05-06 16:30:43 +0600

    refactor: move response handling logic to end of request processing

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -35,23 +35,22 @@ handler.handleReqRes = (req, res) => {
     ? routes[trimmedPath]
     : notFoundHandler;
 
-  chosenHandler(requestProperties, (statusCode, payload) => {
-    statusCode = typeof statusCode === 'number' ? statusCode : 500;
-    payload = typeof payload === 'object' ? payload : {};
-
-    const payloadString = JSON.stringify(payload);
-
-    // return the final response
-    res.writeHead(statusCode);
-    res.end(payloadString);
-  });
-
   req.on('data', (buffer) => {
     realData += decoder.write(buffer);
   });
 
   req.on('end', () => {
     realData += decoder.end();
+    chosenHandler(requestProperties, (statusCode, payload) => {
+      statusCode = typeof statusCode === 'number' ? statusCode : 500;
+      payload = typeof payload === 'object' ? payload : {};
+
+      const payloadString = JSON.stringify(payload);
+
+      // return the final response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    });
     console.log('Request received with this payload: ', realData);
   });
 };
